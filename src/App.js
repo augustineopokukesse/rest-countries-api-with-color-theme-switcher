8f6d5e5dda044ad768644922807e9c0b38406a75
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-06-07 19:51:56 +0000

    created components folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Countries from "./components/Countries";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Countries />
+    </>
+  );
+}
 
 export default App;
