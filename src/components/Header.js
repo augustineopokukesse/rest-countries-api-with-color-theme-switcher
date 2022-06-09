44d3e93c13949833592c8825490dd925ed548586
augustine-ok <augustine.opoku-kesse@amalitech.org>
2022-06-09 13:43:55 +0000

    Complete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
+import "../styles/header.scss"
 function Header(){
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+          <header className="header">
+            <div>
+                <h1>Where in the world?</h1>
+            </div>
+            <div>
+                <h4>Dark Mode</h4>
+            </div>
+          </header>
+        </>
     )
 }
 
