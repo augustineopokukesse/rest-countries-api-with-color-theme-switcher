b227b140e704a6cc92c9126ad37f193bf790454b
augustine-ok <augustine.opoku-kesse@amalitech.org>
2022-06-09 12:36:49 +0000

    Create header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Countries from "./components/all_Countries";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Switch>
         <Route path="/">
           <Countries />
