14fdbc28a8921dc51430c7f1457f5b18eb510fa1
augustine-ok <augustine.opoku-kesse@amalitech.org>
2022-06-09 12:15:01 +0000

    Setup react router for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Countries from "./components/all_Countries";
 
 function App() {
   return (
-    <>
-      <Countries />
-    </>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Countries />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
