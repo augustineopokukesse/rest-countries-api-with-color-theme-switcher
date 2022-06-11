a08c144edc96a2e5c1d8fa3c15409e71104e8b6d
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-06-11 04:25:19 +0000

    fetched country data from site

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Country = () => {
+  const [country, setCountry] = useState([]);
+  const { name } = useParams();
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      const response = await fetch(`https://restcountries.com/v2/name/${name}`);
+      const country = await response.json();
+      setCountry(country);
+      console.log(country);
+    };
+
+    fetchCountry();
+  });
+
   return (
     <>
       <h1>Country Data</h1>
