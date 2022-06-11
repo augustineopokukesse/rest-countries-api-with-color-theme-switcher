fa123300d883173f5881cab032fc16cd40cded94
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-06-11 12:08:26 +0000

    render api info on page

--- src/components/Country.js
+++ src/components/Country.js
@@ -17,4 +17,73 @@ const Country = () => {
 
     fetchCountry();
   });
+
+  //   render infromation on page
+  return (
+    <>
+      <section className="country">
+        {country.map((c) => {
+          const {
+            numericCode,
+            flag,
+            name,
+            nativeName,
+            population,
+            region,
+            subregion,
+            capital,
+            topLevelDomain,
+            currencies,
+            languages,
+            borders,
+          } = c;
+
+          return (
+            <article key={numericCode}>
+              <div className="flag">
+                <img src={flag} alt={name}></img>
+              </div>
+              {/* divs */}
+              <div className="country-details">
+                <div>
+                  <h2>{name}</h2>
+                  <h5>
+                    Native Name: <span>{nativeName}</span>
+                  </h5>
+                  <h5>
+                    Population: <span>{population}</span>
+                  </h5>
+                  <h5>
+                    Region: <span>{region}</span>
+                  </h5>
+                  <h5>
+                    Sub Region: <span>{subregion}</span>
+                  </h5>
+                  <h5>
+                    Capital: <span>{capital}</span>
+                  </h5>
+                </div>
+                <div>
+                  <h5>
+                    Top Level Domain: <span>{topLevelDomain}</span>
+                  </h5>
+                  <h5>
+                    Currencies: <span>{currencies[0].name}</span>
+                  </h5>
+                  <h5>
+                    Languages: <span>{languages[0].name}</span>
+                  </h5>
+                </div>
+                <div>
+                  <h3>Border Countries: {borders}</h3>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </>
+  );
 };
+
+export default Country;
