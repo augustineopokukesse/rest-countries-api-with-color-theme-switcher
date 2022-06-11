d1fcdd4d9297b04d5d88e7947b53d6c94fd6b8c3
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-06-11 05:07:35 +0000

    render information on page

--- src/components/Country.js
+++ src/components/Country.js
@@ -11,7 +11,7 @@ const Country = () => {
       const response = await fetch(`https://restcountries.com/v2/name/${name}`);
       const country = await response.json();
       setCountry(country);
-      console.log(country);
+      //   console.log(country);
     };
 
     fetchCountry();
@@ -19,7 +19,66 @@ const Country = () => {
 
   return (
     <>
-      <h1>Country Data</h1>
+      <section className="country">
+        {country.map((c) => {
+          const {
+            numericCode,
+            flag,
+            name,
+            nativeName,
+            population,
+            region,
+            subregion,
+            capital,
+            topLevelDomain,
+            currencies,
+            languages,
+            borders,
+          } = c;
+
+          return (
+            <article key={numericCode}>
+              <div className="flag">
+                <img src={flag} alt={name}></img>
+              </div>
+              <div className="country-details">
+                <div>
+                  <h2>{name}</h2>
+                  <h5>
+                    Native Name: <span>{nativeName}</span>
+                  </h5>
+                  <h5>
+                    Population: <span>{population}</span>
+                  </h5>
+                  <h5>
+                    Region: <span>{region}</span>
+                  </h5>
+                  <h5>
+                    Sub Region: <span>{subregion}</span>
+                  </h5>
+                  <h5>
+                    Capital: <span>{capital}</span>
+                  </h5>
+                </div>
+                <div>
+                  <h5>
+                    Top Level Domain: <span>{topLevelDomain}</span>
+                  </h5>
+                  <h5>
+                    Currencies: <span>{currencies[0].name}</span>
+                  </h5>
+                  <h5>
+                    Languages: <span>{languages[0].name}</span>
+                  </h5>
+                </div>
+                <div>
+                  <h3>Border Countries: {borders}</h3>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
     </>
   );
 };
