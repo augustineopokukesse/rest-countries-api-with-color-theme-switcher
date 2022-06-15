565b54cbca404c09e213334939d3267a0209352b
augustine-ok <augustine.opoku-kesse@amalitech.org>
2022-06-15 11:24:38 +0000

    Create select filter for regions

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,6 +13,14 @@ function Filter({searchCountries, searchInput}) {
                 onChange={(e) => searchCountries(e.target.value)}
                 autoComplete="off"
                 />
+
+                <select name="select" id="select">
+                    <option value="Africa">Africa</option>
+                    <option value="America">America</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
             </div>
         </>
     )
