644fc9cec2f54f1be92b9ac040dc0b00362d2969
augustine-ok <augustine.opoku-kesse@amalitech.org>
2022-06-15 08:16:04 +0000

    Create search feature in filter component

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+function Filter() {
+    return (
+        <>
+            <input 
+              type="search" 
+              name="search"
+              id="search"
+              placeholder="Search by country name"
+            />
+        </>
+    )
+}
+
+export default Filter;
