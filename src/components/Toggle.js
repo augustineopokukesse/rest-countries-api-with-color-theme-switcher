89eece9c63ae6e6b5f07580b9cfbc39e6468aed2
augustine-ok <augustine.opoku-kesse@amalitech.org>
2022-06-21 15:38:25 +0000

    Fix back button code dark theme

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -6,18 +6,30 @@ function Toggle(){
     const [darkMode, setDarkMode] = useState(false)
 
     const changeTheme = () => {
-
         document.body.classList.toggle("dark");
+
         //document.getElementById("darktheme").classList.toggle("dark");
         //document.getElementsByClassName("modeText")[0].classList.toggle("dark");
         //document.getElementsByClassName("link")[0].classList.toggle("dark");
+
         const allClasses = document.getElementsByClassName("darktheme");
         for (let i = 0; i < allClasses.length; i++){
             allClasses[i].classList.toggle("dark");
         }
         //document.getElementById("darkthemeCountries").classList.toggle("dark");
         setDarkMode(!darkMode);
-        // console.log(allClasses)
+        // const allClasses = document.getElementsByClassName("darktheme");
+        // if (darkMode) {
+        //     document.body.classList.add("dark");
+        //     for (let i = 0; i < allClasses.length; i++){
+        //         allClasses[i].classList.add("dark");
+        //     }
+        // } else {
+        //     document.body.classList.remove("dark");
+        //     for (let i = 0; i < allClasses.length; i++){
+        //         allClasses[i].classList.remove("dark");
+        //     }
+        // }
     }
     return (
         <>
